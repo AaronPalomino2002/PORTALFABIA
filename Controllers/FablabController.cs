d4ef1bda7957e933186d791ffcfc81cac6b34537
AaronPalomino2002 <palominofalconaaron@gmail.com>
2025-02-14 19:15:06 -0500

    fablab

---
+++ Controllers/FablabController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PORTALFABIA.Controllers
+{
+   
+    public class FablabController : Controller
+    {
+        private readonly ILogger<FablabController> _logger;
+
+        public FablabController(ILogger<FablabController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
